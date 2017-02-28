bdfe8617f24041483c2ed15af82db38e9fa257c3
mustafazc <mustafa.zcmp@gmail.com>
2017-02-28 20:30:59 +0400

    API.AI require

--- app.js
+++ app.js
@@ -5,17 +5,12 @@ const bodyParser = require('body-parser')
 const request = require('request')
 const app = express()
 const _ = require('underscore');
+const apiai = require('apiai');
 
 // Package and API key for api.ai
-const apiai = require('apiai');
 const apiaiApp = apiai(process.env.API_AI);
 
 app.set('port', (process.env.PORT || 5000))
-
-
-
-
-
 
 // Mongoose Connection
 var mongoose = require('mongoose');
