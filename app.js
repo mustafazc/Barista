d50411196a0e8388520b66c7a87cfe8f9b3c23f9
mustafazc <mustafa.zcmp@gmail.com>
2017-02-28 20:37:55 +0400

    Re-Added Gitignore

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 'use strict'
 
-let express = require('express')
-let bodyParser = require('body-parser')
-let request = require('request')
-let app = express()
-let _ = require('underscore');
-let apiai = require('apiai');
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const app = express()
+const _ = require('underscore');
+const apiai = require('apiai');
 
 // Package and API key for api.ai
-let apiaiApp = apiai(process.env.API_AI);
+const apiaiApp = apiai(process.env.API_AI);
 
 app.set('port', (process.env.PORT || 5000))
 
